7cb4c6b24f79dee0d6b33feedecdce80e5525dc9
Nick <vo.nick2868@gmail.com>
2022-06-03 01:27:27 -0700

    Update reddit-scrape.py

--- reddit-scrape.py
+++ reddit-scrape.py
@@ -1,6 +1,14 @@
 from RedDownloader import RedDownloader
 import praw
 
+directory = "D:/Reddit-Scrape/"
+# Path
+path = os.path.join(directory, subreddit_name)
+isDirExist = os.path.exists(path)
+
+if isDirExist == False:
+  os.mkdir(path)
+  
 subreddit_name = input("Enter subreddit: ")
 url = "https://www.reddit.com"
 
@@ -12,7 +20,6 @@ reddit = praw.Reddit(client_id=CLIENT_ID,
 hot_posts = reddit.subreddit(subreddit_name).new(limit=100)
 for post in hot_posts:
   permalink = post.permalink
-  print(url + permalink)
   title = post.title
-  file = RedDownloader.Download(url + permalink, output = title , quality = 720, destination = "D:/Python-Project/" + subreddit_name)
+  file = RedDownloader.Download(url + permalink, output = title , quality = 720, destination = path + "/")
 
