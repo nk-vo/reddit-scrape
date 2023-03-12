fd886296304659cb5ae2d1737a86dedcea809878
Nick <vo.nick2868@gmail.com>
2023-03-11 17:50:15 -0800

    Add files via upload

--- reddit-scrape.py
+++ reddit-scrape.py
@@ -2,31 +2,78 @@ from RedDownloader import RedDownloader
 import praw
 import os
 import re
+import sys
+import tkinter
+from tkinter import ttk
+from dotenv import load_dotenv
+load_dotenv()
 
-subreddit_name = input("Enter subreddit: ")
-url = "https://www.reddit.com"
+subreddit_list = os.getenv("SUBREDDIT_LIST").split(",")
 
-directory = DESIRED_DIRECTORY
-# Path
-path = os.path.join(directory, subreddit_name)
+def clean_filename(filename, limit=260):
+    """
+    Remove invalid characters, URLs, trim, and reduce filename length
+    """
+    # Remove any URLs from the filename
+    url_pattern = re.compile(
+        r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    filename = url_pattern.sub('', filename)
 
-if not os.path.exists(path):
-  os.mkdir(path)
+    # Remove any invalid characters from the filename
+    invalid_chars = re.compile(r'[\\/:*?"<>|]')
+    filename = invalid_chars.sub('', filename)
 
-#get those parameters from reddit developer account
-reddit = praw.Reddit(client_id=CLIENT_ID,
-                     client_secret=CLIENT_SECRET,
-                     user_agent=USER_AGENT)
+    # Trim the filename
+    filename = filename.strip()
 
-hot_posts = reddit.subreddit(subreddit_name).new(limit=10000)
-count = 0
-for post in hot_posts:
-  permalink = post.permalink
-  title = post.title
-  title = re.sub(r'[^A-Za-z0-9 ,]+', '', title).strip()
-  
-  if title == "":
-    title = str(count)
-    count += 1
-  if not os.path.exists(path + "/" + title + (".jpeg" or ".mp4")):
-    file = RedDownloader.Download(url + permalink, output = title , quality = 720, destination = path + "/")
+    # Reduce filename length if it exceeds the limit
+    if len(filename) > limit:
+        filename = filename[:limit]
+
+    return filename
+
+
+for subreddit_name in subreddit_list:
+    post_num = 1000  # Set the number of posts to download
+    url = "https://www.reddit.com"
+
+    directory = "E:/Reddit-Scrape/"
+    # Path
+    path = os.path.join(directory, subreddit_name)
+
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    reddit = praw.Reddit(client_id=os.getenv("CLIENT_ID"),
+                         client_secret=os.getenv("CLIENT_SECRET"),
+                         user_agent=os.getenv("USER_AGENT"))
+    try:
+        hot_posts = reddit.subreddit(subreddit_name).new(limit=int(post_num))
+        count = 0
+        post_downloaded = 0
+        for post in hot_posts:
+            permalink = post.permalink
+            title = post.title
+            title = clean_filename(title)
+
+            if title == "":
+                while (os.path.exists(path + "/" + str(count) + ".mp4")):
+                    count += 1
+                title = str(count)
+            if not os.path.exists(path + "/" + title + ".mp4"):
+                if os.path.exists(path + "/" + title + ".jpeg"):
+                    continue
+                file = RedDownloader.Download(
+                    url + permalink,
+                    output=title,
+                    quality=720,
+                    destination=path + "/")
+                post_downloaded += 1
+
+            if os.path.exists(path + "/" + title + ".jpeg"):
+                continue
+    except Exception as e:
+        print(f"Error while downloading media from {subreddit_name}: {e}")
+        continue
+    print("Downloaded " + str(post_downloaded) +
+          " posts from r/" + subreddit_name)
