a47789e03eda9f7f49718aad8ceb4345268002db
Nick <vo.nick2868@gmail.com>
2023-03-13 10:39:16 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# reddit-scrape
-A Reddit Scraping Script especially for videos and images using Praw and RedDownloader APIs
+# README
+
+This Python script downloads the media from the latest posts of the specified subreddits using the RedDownloader module and the PRAW API wrapper for Reddit. The downloaded media is stored in directories named after the subreddit, and the title of the post is used as the filename. If a post has already been downloaded, it is skipped.
+
+The script also keeps track of the downloaded posts for each subreddit in a JSON file, so that previously downloaded media is not downloaded again.
+
+## Requirements
+
+Python 3
+PRAW API wrapper for Reddit (praw module)
+RedDownloader module (RedDownloader module)
+dotenv module (python-dotenv module)
+
+## Setup
+
+Clone or download the script to a directory on your computer.
+Install the required modules using pip.
+Create a .env file in the same directory as the script with the following variables:
+CLIENT_ID: Reddit API client ID
+CLIENT_SECRET: Reddit API client secret
+USER_AGENT: Reddit API user agent
+SUBREDDIT_LIST: Comma-separated list of subreddits to scrape
+MEDIA_DIRECTORY: Directory to save downloaded media
+Run the script in a Python environment.
+
+## Usage
+
+To use the script, simply run it in a Python environment. The script will scrape the latest posts from the specified subreddits, download any new media, and store the downloaded media in the specified directory.
+
+The downloaded media is saved in directories named after the subreddit, with the title of the post used as the filename. If a post has already been downloaded, it is skipped.
+
+The script also keeps track of the downloaded posts for each subreddit in a JSON file, so that previously downloaded media is not downloaded again.
+
+## Note
+
+The Reddit API has a rate limit of 60 requests per minute. If you encounter any errors while using the script, try reducing the number of posts to download or adding a delay between requests.
--- reddit-scrape.py
+++ reddit-scrape.py
@@ -6,8 +6,6 @@ import datetime
 import json
 from dotenv import load_dotenv
 
-from scrape_subreddit_posts import scrape_subreddit_posts
-
 load_dotenv()
 
 subreddit_list = os.getenv("SUBREDDIT_LIST").split(",")
@@ -36,56 +34,58 @@ def clean_filename(filename, limit=259):
 
 
 def download_posts():
-    start_time = datetime.datetime.now()
-    url = "https://www.reddit.com"
-    post_num = 1000  # Set the number of posts to download
-    media_directory = os.getenv("MEDIA_DIRECTORY")
-    # Path to JSON file that stores the downloaded posts for each subreddit
-    posts_json_path = os.path.join(media_directory, "posts.json")
+  start_time = datetime.datetime.now()
+  url = "https://www.reddit.com"
+  post_num = 1000  # Set the number of posts to download
+  media_directory = os.getenv("MEDIA_DIRECTORY")
+  # Path to JSON file that stores the downloaded posts for each subreddit
+  source_directory = os.getenv("SOURCE_DIRECTORY")
+  posts_json_path = os.path.join(source_directory, "posts.json")
 
-    data = {}
-    if os.path.exists(posts_json_path):
-        with open(posts_json_path) as f:
-            data = json.load(f)
-
-    reddit = praw.Reddit(client_id=os.getenv("CLIENT_ID"),
-                         client_secret=os.getenv("CLIENT_SECRET"),
-                         user_agent=os.getenv("USER_AGENT"))
-
-    for subreddit_name in subreddit_list:
-        if subreddit_name not in data:
-            data[subreddit_name] = []
-        hot_posts = reddit.subreddit(subreddit_name).new(limit=int(post_num))
-        for post in hot_posts:
-            title = post.title
-            created_utc = post.created_utc
-            if not any(d['title'] == title for d in data[subreddit_name]):
-                data[subreddit_name].append({"title": title, "created_utc": int(created_utc)})
-                title_cleaned = clean_filename(title)
-                output = f"{title_cleaned}_{int(created_utc)}"
-                path = os.path.join(media_directory, subreddit_name)
-                if not os.path.exists(path):
-                    os.mkdir(path)
-                try:
-                    file = RedDownloader.Download(url + post.permalink, output=output, destination=path + "/")
-                    print(f"Downloaded {title_cleaned}")
-                except Exception as e:
-                    print(f"Error while downloading media from {subreddit_name}: {e}")
-                    continue
-
+  data = {}
+  if os.path.exists(posts_json_path):
+    with open(posts_json_path) as f:
+      data = json.load(f)
+  else:
     with open(posts_json_path, "w") as f:
-        json.dump(data, f, indent=2)
+      json.dump(data, f, indent=2)
+      
+  reddit = praw.Reddit(client_id=os.getenv("CLIENT_ID"),
+                        client_secret=os.getenv("CLIENT_SECRET"),
+                        user_agent=os.getenv("USER_AGENT"))
 
-    end_time = datetime.datetime.now()
-    time_elapsed = end_time - start_time
-    print("Posts downloaded:")
-    for subreddit_name, post_data in data.items():
-        post_count = len(post_data)
-        print(f"r/{subreddit_name}: {post_count}")
+  for subreddit_name in subreddit_list:
+    if subreddit_name not in data:
+      data[subreddit_name] = []
+    hot_posts = reddit.subreddit(subreddit_name).new(limit=int(post_num))
+    for post in hot_posts:
+      title = post.title
+      created_utc = post.created_utc
+      if not any(d['title'] == title and d['created_utc'] == int(created_utc) for d in data[subreddit_name]):
+        data[subreddit_name].append({"title": title, "created_utc": int(created_utc)})
+        title_cleaned = clean_filename(title)
+        output = f"{title_cleaned}_{int(created_utc)}"
+        path = os.path.join(media_directory, subreddit_name)
+        if not os.path.exists(path):
+          os.mkdir(path)
+        try:
+          RedDownloader.Download(url + post.permalink, output=output, destination=path + "/")
+        except Exception as e:
+          print(f"Error while downloading media from {subreddit_name}: {e}")
+          continue
 
-    print(f"Time elapsed: {time_elapsed}")
+  with open(posts_json_path, "w") as f:
+      json.dump(data, f, indent=2)
+
+  end_time = datetime.datetime.now()
+  time_elapsed = end_time - start_time
+  print("Posts downloaded:")
+  for subreddit_name, post_data in data.items():
+      post_count = len(post_data)
+      print(f"r/{subreddit_name}: {post_count}")
+
+  print(f"Time elapsed: {time_elapsed}")
 
 
 if __name__ == "__main__":
-  scrape_subreddit_posts()
   download_posts()
